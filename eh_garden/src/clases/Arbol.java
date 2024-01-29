b7ded6426cdc1ea51bf06614b131aba7dd214c8d
Julen Garcia <ikctk@plaiaundi.net>
2024-01-29 13:33:53 +0100

    crear constructores y datos

---
+++ eh_garden/src/clases/Arbol.java
@@ -0,0 +1,27 @@
+package clases;
+
+public class Arbol {
+
+	private int id;
+	private String nombreComun;
+	private String nombreCientifico;
+	private String habitat;
+	private int altura;
+	private String origen;
+	
+	public Arbol(int id, String nombreComun, String nombreCientifico, String habitat, int altura, String origen) {
+		super();
+		this.id = id;
+		this.nombreComun = nombreComun;
+		this.nombreCientifico = nombreCientifico;
+		this.habitat = habitat;
+		this.altura = altura;
+		this.origen = origen;
+	}
+	
+	public Arbol() {
+		
+	}
+	
+	
+}
