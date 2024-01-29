5c273251e361cc0199ab5374ec1d9fc2dc3986f0
Julen Garcia <ikctk@plaiaundi.net>
2024-01-29 13:47:24 +0100

    esqueleto del switch case

--- eh_garden/src/main/GestorArboles.java
+++ eh_garden/src/main/GestorArboles.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import clases.Arbol;
 
@@ -13,25 +14,42 @@ public class GestorArboles {
 
 	public static void run() {
 		
+		Scanner scan = new Scanner(System.in);
+
 		ArrayList<Arbol> arboles = new ArrayList<Arbol>();
 		
 		final int SELECT = 1;
 		final int INSERT = 2;
 		final int UPDATE = 3;
 		final int DELETE = 4;
+		final int SALIR = 0;
 		
 		int intro = 1;
 		
-		System.out.println("---MENU---");
-		System.out.println(SELECT + " - Mostrar por pantalla");
-		System.out.println(INSERT + " - Insertar tupla");
-		System.out.println(UPDATE + " - Actueñizar tupla");
-		System.out.println(DELETE + " - Eliminar tupla");
-		
-		
 		do {
+			System.out.println("---MENU---");
+			System.out.println(SELECT + " - Mostrar por pantalla");
+			System.out.println(INSERT + " - Insertar tupla");
+			System.out.println(UPDATE + " - Actueñizar tupla");
+			System.out.println(DELETE + " - Eliminar tupla");
+			
+			intro = Integer.parseInt(scan.nextLine());
+			
 			switch (intro) {
 			case SELECT:
+				
+				break;
+			case INSERT:
+				
+				break;
+			case UPDATE:
+				
+				break;
+			case DELETE:
+				
+				break;
+				
+			case SALIR:
 				
 				break;
 
