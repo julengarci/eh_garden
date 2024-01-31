37dd522f924610dff1d6ef58458ddd2f95575ae1
Julen Garcia <ikctk@plaiaundi.net>
2024-01-31 13:52:24 +0100

    funciona

--- eh_garden/src/clases/Habitat.java
+++ eh_garden/src/clases/Habitat.java
@@ -18,6 +18,10 @@ public class Habitat {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	@Override
+	public String toString() {
+		return "id=" + id + ", nombre=" + nombre;
+	}
 	
 	
 }
